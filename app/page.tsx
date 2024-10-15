7450154df523596afa6c4e7d15305469ab40135e
Andres Hung <andres.hung@outlook.com>
2024-10-15 14:59:48 -0600

    Add an alert to the front page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
 import "@/styles/main.css";
 
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+
 import FrontCarousel from "@/components/front-carousel";
 
 export default async function Home() {
   return (
     <>
       <h1>The University of Belize TreeTrack Project</h1>
+      <Alert className="mt-4 opacity-90">
+        <AlertTitle>Important Note!</AlertTitle>
+        <AlertDescription>
+          UB TreeTrack is currently a prototype in active development. UI and
+          functionality may change at any time.
+        </AlertDescription>
+      </Alert>
       <FrontCarousel />
     </>
   );
