67db9a5eb1be684b54ea2f4c9c8ee66753f42023
Andres Hung <andres.hung@outlook.com>
2024-10-15 11:27:47 -0600

    Enforce unique scientific names

--- app/admin/add/page.tsx
+++ app/admin/add/page.tsx
@@ -1,12 +1,28 @@
 import "@/styles/main.css";
 
+import { createSupabaseServerComponentClient } from "@/lib/supabase/server";
 import PlantAddForm from "@/components/add-form";
 
-export default function AddPlant() {
+export default async function AddPlant() {
+  const supabase = createSupabaseServerComponentClient();
+
+  const { data: plants, error } = await supabase
+    .from("plants")
+    .select("scientific_name");
+
+  if (error) {
+    console.error("Error fetching scientific names:", error);
+  }
+
+  const scientificNamesArray =
+    plants?.map(
+      (plant: { scientific_name: string }) => plant.scientific_name,
+    ) || [];
+
   return (
     <>
       <h1>Add Plant</h1>
-      <PlantAddForm />
+      <PlantAddForm existingNames={scientificNamesArray} />
     </>
   );
 }
--- components/add-form.tsx
+++ components/add-form.tsx
@@ -31,29 +31,38 @@ import { addSupabaseEntry } from "@/actions";
 const MAX_FILE_SIZE = 10 * 1024 * 1024; // 10MB
 const ACCEPTED_FILE_TYPES = ["image/jpeg", "image/png", "image/webp"];
 
-const FormSchema = z.object({
-  scientificName: z.string().min(1, {
-    message: "Scientific name is required",
-  }),
-  commonNames: z.string().min(1, {
-    message: "Common name(s) is required",
-  }),
-  plantDescription: z.string().min(1, {
-    message: "Plant description is required",
-  }),
-  imageFile: z
-    .instanceof(File, { message: "Image file is required" })
-    .refine((file) => ACCEPTED_FILE_TYPES.includes(file.type), {
-      message: "Only JPG, JPEG, PNG, and WEBP files are accepted.",
-    })
-    .refine((file) => file.size <= MAX_FILE_SIZE, {
-      message: "File size must be less than 10MB.",
-    }),
-});
-
-export default function PlantAddForm() {
+export default function PlantAddForm({
+  existingNames,
+}: {
+  existingNames: string[];
+}) {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const router = useRouter();
+
+  const FormSchema = z.object({
+    scientificName: z
+      .string()
+      .min(1, {
+        message: "Scientific name is required",
+      })
+      .refine((name) => !existingNames.includes(name), {
+        message: "Scientific name already exists",
+      }),
+    commonNames: z.string().min(1, {
+      message: "Common name(s) is required",
+    }),
+    plantDescription: z.string().min(1, {
+      message: "Plant description is required",
+    }),
+    imageFile: z
+      .instanceof(File, { message: "Image file is required" })
+      .refine((file) => ACCEPTED_FILE_TYPES.includes(file.type), {
+        message: "Only JPG, JPEG, PNG, and WEBP files are accepted.",
+      })
+      .refine((file) => file.size <= MAX_FILE_SIZE, {
+        message: "File size must be less than 10MB.",
+      }),
+  });
 
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
