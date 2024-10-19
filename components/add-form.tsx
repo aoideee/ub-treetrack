196ac10a23cc0b835ecc09ab1848511e6730e107
Andres Hung <andres.hung@outlook.com>
2024-10-19 15:00:26 -0600

    Remove WEBP from accepted file types

--- components/add-form.tsx
+++ components/add-form.tsx
@@ -32,7 +32,7 @@ import QRCode from "qrcode";
 // zod schema and constraints
 
 const MAX_FILE_SIZE = 10 * 1024 * 1024; // 10MB
-const ACCEPTED_FILE_TYPES = ["image/jpeg", "image/png", "image/webp"];
+const ACCEPTED_FILE_TYPES = ["image/jpeg", "image/png"];
 
 export default function PlantAddForm({
   existingNames,
@@ -60,7 +60,7 @@ export default function PlantAddForm({
     imageFile: z
       .instanceof(File, { message: "Image file is required" })
       .refine((file) => ACCEPTED_FILE_TYPES.includes(file.type), {
-        message: "Only JPG, JPEG, PNG, and WEBP files are accepted.",
+        message: "Only JPG, JPEG, and PNG files are accepted.",
       })
       .refine((file) => file.size <= MAX_FILE_SIZE, {
         message: "File size must be less than 10MB.",
@@ -269,7 +269,7 @@ export default function PlantAddForm({
                 />
               </FormControl>
               <FormDescription>
-                Accepted image file formats: JPG, JPEG, PNG, WEBP
+                Accepted image file formats: JPG, JPEG, PNG
               </FormDescription>
               <FormDescription>Max file size: 10MB</FormDescription>
               <FormMessage />
